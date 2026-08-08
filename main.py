64f4d1f237adc740d213705f53449c40bef96473
nouur-mohaamed <nouurmohaamed@icloud.com>
2026-08-08 10:39:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,24 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
 import smtplib
 import os
-
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
-
-today = datetime.now()
-today_tuple = (today.month, today.day)
-
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
-
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+import dotenv
+from email.message import EmailMessage
+from datetime import datetime as dt
+import random
+today_date=dt.today()
+to_email = "Salwa.maher@acg-eg.com"
+from_email = "nouurmohaamed6777@gmail.com"
+dotenv.load_dotenv()
+password =str(os.getenv("password"))
+if today_date.hour==10:
+    with smtplib.SMTP("smtp.gmail.com",587) as connection:
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+        connection.login(user=from_email,password=password)
+        msg=EmailMessage()
+        msg["from"]=from_email
+        msg["to"]=to_email
+        msg["subject"]="Salwa's everyday's Morning message"
+        with open("message.txt") as file:
+            morning_messages=file.readlines()
+        msg.set_content(random.choice(morning_messages))
+        connection.send_message(msg)
+    print('sent successfully')
