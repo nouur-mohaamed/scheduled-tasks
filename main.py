4753b263d5746863bd64bfa05a33da375efddccd
nouur-mohaamed <nouurmohaamed@icloud.com>
2026-08-08 10:49:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,15 @@ to_email = "Salwa.maher@acg-eg.com"
 from_email = "nouurmohaamed6777@gmail.com"
 dotenv.load_dotenv()
 password =str(os.getenv("password"))
-if today_date.hour==10:
-    with smtplib.SMTP("smtp.gmail.com",587) as connection:
-        connection.starttls()
-        connection.login(user=from_email,password=password)
-        msg=EmailMessage()
-        msg["from"]=from_email
-        msg["to"]=to_email
-        msg["subject"]="Salwa's everyday's Morning message"
-        with open("message.txt") as file:
-            morning_messages=file.readlines()
-        msg.set_content(random.choice(morning_messages))
-        connection.send_message(msg)
-    print('sent successfully')
+with smtplib.SMTP("smtp.gmail.com",587) as connection:
+    connection.starttls()
+    connection.login(user=from_email,password=password)
+    msg=EmailMessage()
+    msg["from"]=from_email
+    msg["to"]=to_email
+    msg["subject"]="Salwa's everyday's Morning message"
+    with open("message.txt") as file:
+        morning_messages=file.readlines()
+    msg.set_content(random.choice(morning_messages))
+    connection.send_message(msg)
+    
